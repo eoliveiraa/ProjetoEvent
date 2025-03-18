77d981327bd21ed2899b7136d118abcb6bcdc35c
Emilly Oliveira <emilly.o.santos7@aluno.senai.br>
2025-03-18 15:59:06 -0300

    feat:Atualizando Api EventPlus

---
+++ Repositories/UsuarioRepository.cs
@@ -0,0 +1,42 @@
+﻿using Event_.Context;
+using Event_.Domains;
+using Event_.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Event_.Repositories
+{
+    public class UsuarioRepository : IUsuarioRepository
+    {
+
+        private readonly Event_Context _Context;
+
+        public Usuario BuscarPorEmailESenha(string email, string senha)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Usuario BuscarPorId(Guid id)
+        {
+            try
+            {
+                Usuario usuarioBuscado = _Context.Usuario.Find(id)!;
+
+                if (usuarioBuscado != null)
+                {
+                    return usuarioBuscado;
+                }
+                return null!;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public void Cadastrar(Usuario novousuario)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
